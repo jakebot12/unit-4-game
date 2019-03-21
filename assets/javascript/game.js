a3dd1c2ca26e4afcb41a41a031e4aaeafb610e78
Jake Gurley <jakemg12@gmail.com>
2019-03-20 22:46:33 -0700

    <COMMENT>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,10 @@ $(document).ready(function () {
     var crystalTwo;
     var crystalThree;
     var crystalFour;
-
+    var randomNum1 = Math.floor(Math.random() * 12) + 1;
+    var randomNum2 = Math.floor(Math.random() * 12) + 1;
+    var randomNum3 = Math.floor(Math.random() * 12) + 1;
+    var randomNum4 = Math.floor(Math.random() * 12) + 1;
 
 
 
@@ -16,17 +19,17 @@ $(document).ready(function () {
 
     $("#target-score").text(targetScore);
 
-    var randomNum1 = Math.floor(Math.random() * 12) + 1;
-    var randomNum2 = Math.floor(Math.random() * 12) + 1;
-    var randomNum3 = Math.floor(Math.random() * 12) + 1;
-    var randomNum4 = Math.floor(Math.random() * 12) + 1;
+    function newRandomNumbers() {
+        randomNum1 = Math.floor(Math.random() * 12) + 1;
+        randomNum2 = Math.floor(Math.random() * 12) + 1;
+        randomNum3 = Math.floor(Math.random() * 12) + 1;
+        randomNum4 = Math.floor(Math.random() * 12) + 1;
+    }
+
     var wins = 0;
     var losses = 0;
 
-    $('#redGem').on("click", function () {
-        crystalOne = randomNum1;
-        playerScore += crystalOne;
-
+    function checkWin() {
         if (playerScore == targetScore) {
             wins++;
             $('#wins').text(wins);
@@ -34,10 +37,7 @@ $(document).ready(function () {
             $('#player-score').text(playerScore);
             targetScore = Math.floor(Math.random() * 101) + 19;
             $("#target-score").text(targetScore);
-            randomNum1 = Math.floor(Math.random() * 12) + 1;
-            randomNum2 = Math.floor(Math.random() * 12) + 1;
-            randomNum3 = Math.floor(Math.random() * 12) + 1;
-            randomNum4 = Math.floor(Math.random() * 12) + 1;
+            newRandomNumbers();
             $('#wins').text(wins);
             $('#losses').text(losses);
             console.log("Crystal One: " + crystalOne);
@@ -52,10 +52,7 @@ $(document).ready(function () {
             $('#player-score').text(playerScore);
             targetScore = Math.floor(Math.random() * 101) + 19;
             $("#target-score").text(targetScore);
-            randomNum1 = Math.floor(Math.random() * 12) + 1;
-            randomNum2 = Math.floor(Math.random() * 12) + 1;
-            randomNum3 = Math.floor(Math.random() * 12) + 1;
-            randomNum4 = Math.floor(Math.random() * 12) + 1;
+            newRandomNumbers();
             $('#wins').text(wins);
             $('#losses').text(losses);
             console.log("Crystal One: " + crystalOne);
@@ -71,150 +68,32 @@ $(document).ready(function () {
             console.log("Player Score: " + playerScore);
             console.log("Target Score: " + targetScore);
         }
+    }
+    $('#redGem').on("click", function () {
+        crystalOne = randomNum1;
+        playerScore += crystalOne;
+        checkWin();
+
     });
 
     $('#blueGem').on("click", function () {
         crystalTwo = randomNum2;
         playerScore += crystalTwo;
-        if (playerScore == targetScore) {
-            wins++;
-            $('#wins').text(wins);
-            playerScore = 0;
-            $('#player-score').text(playerScore);
-            targetScore = Math.floor(Math.random() * 101) + 19;
-            $("#target-score").text(targetScore);
-            randomNum1 = Math.floor(Math.random() * 12) + 1;
-            randomNum2 = Math.floor(Math.random() * 12) + 1;
-            randomNum3 = Math.floor(Math.random() * 12) + 1;
-            randomNum4 = Math.floor(Math.random() * 12) + 1;
-            $('#wins').text(wins);
-            $('#losses').text(losses);
-            console.log("Crystal Two: " + crystalTwo);
-            console.log("Player Score: " + playerScore);
-            console.log("Target Score: " + targetScore);
-            alert("There's a WIN!!!");
-        } else if (playerScore >= targetScore) {
-            losses++;
-            $('#losses').text(losses);
-            playerScore = 0;
-            $('#player-score').text(playerScore);
-            targetScore = Math.floor(Math.random() * 101) + 19;
-            $("#target-score").text(targetScore);
-            randomNum1 = Math.floor(Math.random() * 12) + 1;
-            randomNum2 = Math.floor(Math.random() * 12) + 1;
-            randomNum3 = Math.floor(Math.random() * 12) + 1;
-            randomNum4 = Math.floor(Math.random() * 12) + 1;
-            $('#wins').text(wins);
-            $('#losses').text(losses);
-            console.log("Crystal Two: " + crystalTwo);
-            console.log("Player Score: " + playerScore);
-            console.log("Target Score: " + targetScore);
-            alert("Doooh! You took a loss! Try again!");
-        } else {
-            $("#player-score").text(playerScore);
-            console.log(playerScore);
-            $('#wins').text(wins);
-            $('#losses').text(losses);
-            console.log("Crystal Two: " + crystalTwo);
-            console.log("Player Score: " + playerScore);
-            console.log("Target Score: " + targetScore);
-        }
+        checkWin();
+
     });
 
     $('#yellowGem').on("click", function () {
         crystalThree = randomNum3;
         playerScore += crystalThree;
-        if (playerScore == targetScore) {
-            wins++;
-            $('#wins').text(wins);
-            playerScore = 0;
-            $('#player-score').text(playerScore);
-            targetScore = Math.floor(Math.random() * 101) + 19;
-            $("#target-score").text(targetScore);
-            randomNum1 = Math.floor(Math.random() * 12) + 1;
-            randomNum2 = Math.floor(Math.random() * 12) + 1;
-            randomNum3 = Math.floor(Math.random() * 12) + 1;
-            randomNum4 = Math.floor(Math.random() * 12) + 1;
-            $('#wins').text(wins);
-            $('#losses').text(losses);
-            console.log("Crystal Three: " + crystalThree);
-            console.log("Player Score: " + playerScore);
-            console.log("Target Score: " + targetScore);
-            alert("There's a WIN!!!");
-        } else if (playerScore >= targetScore) {
-            losses++;
-            $('#losses').text(losses);
-            playerScore = 0;
-            $('#player-score').text(playerScore);
-            targetScore = Math.floor(Math.random() * 101) + 19;
-            $("#target-score").text(targetScore);
-            randomNum1 = Math.floor(Math.random() * 12) + 1;
-            randomNum2 = Math.floor(Math.random() * 12) + 1;
-            randomNum3 = Math.floor(Math.random() * 12) + 1;
-            randomNum4 = Math.floor(Math.random() * 12) + 1;
-            $('#wins').text(wins);
-            $('#losses').text(losses);
-            console.log("Crystal Three: " + crystalThree);
-            console.log("Player Score: " + playerScore);
-            console.log("Target Score: " + targetScore);
-            alert("Doooh! You took a loss! Try again!");
-        } else {
-            $("#player-score").text(playerScore);
-            console.log(playerScore);
-            $('#wins').text(wins);
-            $('#losses').text(losses);
-            console.log("Crystal Three: " + crystalThree);
-            console.log("Player Score: " + playerScore);
-            console.log("Target Score: " + targetScore);
-        }
+        checkWin();
     });
 
     $('#greenGem').on("click", function () {
         crystalFour = randomNum4;
         playerScore += crystalFour;
+        checkWin()
 
-        if (playerScore == targetScore) {
-            wins++;
-            playerScore = 0;
-            $('#player-score').text(playerScore);
-            targetScore = Math.floor(Math.random() * 101) + 19;
-            $("#target-score").text(targetScore);
-            randomNum1 = Math.floor(Math.random() * 12) + 1;
-            randomNum2 = Math.floor(Math.random() * 12) + 1;
-            randomNum3 = Math.floor(Math.random() * 12) + 1;
-            randomNum4 = Math.floor(Math.random() * 12) + 1;
-            $('#wins').text(wins);
-            $('#losses').text(losses);
-            console.log("Crystal Four: " + crystalFour);
-            console.log("Player Score: " + playerScore);
-            console.log("Target Score: " + targetScore);
-            alert("There's a WIN!!!");
-        } else if (playerScore >= targetScore) {
-            losses++;
-            playerScore = 0;
-            $('#player-score').text(playerScore);
-            targetScore = Math.floor(Math.random() * 101) + 19;
-            $("#target-score").text(targetScore);
-            randomNum1 = Math.floor(Math.random() * 12) + 1;
-            randomNum2 = Math.floor(Math.random() * 12) + 1;
-            randomNum3 = Math.floor(Math.random() * 12) + 1;
-            randomNum4 = Math.floor(Math.random() * 12) + 1;
-            console.log("Crystal Four: " + crystalFour);
-            console.log("Player Score: " + playerScore);
-            console.log("Target Score: " + targetScore);
-            $('#wins').text(wins);
-            $('#losses').text(losses);
-            alert("Doooh! You took a loss! Try again!");
-        } else {
-            $("#player-score").text(playerScore);
-            console.log(playerScore);
-            $('#wins').text(wins);
-            $('#losses').text(losses);
-            console.log("Crystal Four: " + crystalFour);
-            console.log("Player Score: " + playerScore);
-            console.log("Target Score: " + targetScore);
-
-        }
     });
 
 });
